ce767704e350ce24009c7d22cf3d196b37b7e62b
pubuzhixing8 <pubuzhixing@gmail.com>
2024-07-05 09:49:59 +0800

    feat(draw-toolbar): add shape and arrow panel for draw-toolbar #10

---
+++ packages/drawnix/src/components/toolbar/arrow-panel.tsx
@@ -0,0 +1,69 @@
+import classNames from 'classnames';
+import { Island } from '../island';
+import Stack from '../stack';
+import { ToolButton } from '../tool-button';
+import { StraightArrowIcon, ElbowArrowIcon, CurveArrowIcon } from '../icons';
+import { useBoard } from '@plait/react-board';
+import { BoardTransforms } from '@plait/core';
+import React from 'react';
+import { BoardCreationMode, setCreationMode } from '@plait/common';
+import { ArrowLineShape, DrawPointerType } from '@plait/draw';
+
+export interface ArrowProps {
+  icon: React.ReactNode;
+  title: string;
+  pointer: ArrowLineShape;
+}
+
+const ARROWS: ArrowProps[] = [
+  {
+    icon: StraightArrowIcon,
+    title: 'Straight Arrow Line',
+    pointer: ArrowLineShape.straight,
+  },
+  {
+    icon: ElbowArrowIcon,
+    title: 'Elbow Arrow Line',
+    pointer: ArrowLineShape.elbow,
+  },
+  {
+    icon: CurveArrowIcon,
+    title: 'Curve Arrow Line',
+    pointer: ArrowLineShape.curve,
+  },
+];
+
+export type ArrowPanelProps = {
+  onPointerUp: (pointer: DrawPointerType) => void;
+};
+
+export const ArrowPanel: React.FC<ArrowPanelProps> = ({ onPointerUp }) => {
+  const board = useBoard();
+  return (
+    <Island padding={1}>
+      <Stack.Row gap={1}>
+        {ARROWS.map((arrow, index) => {
+          return (
+            <ToolButton
+              key={index}
+              className={classNames({ fillable: false })}
+              type="icon"
+              size={'small'}
+              visible={true}
+              icon={arrow.icon}
+              title={arrow.title}
+              aria-label={arrow.title}
+              onPointerDown={() => {
+                setCreationMode(board, BoardCreationMode.drawing);
+                BoardTransforms.updatePointerType(board, arrow.pointer);
+              }}
+              onPointerUp={() => {
+                onPointerUp(arrow.pointer);
+              }}
+            />
+          );
+        })}
+      </Stack.Row>
+    </Island>
+  );
+};
---
+++ packages/drawnix/src/components/toolbar/shape-panel.tsx
@@ -0,0 +1,110 @@
+import classNames from 'classnames';
+import { Island } from '../island';
+import Stack from '../stack';
+import { ToolButton } from '../tool-button';
+import {
+  RectangleIcon,
+  EllipseIcon,
+  TriangleIcon,
+  DiamondIcon,
+  ParallelogramIcon,
+  RoundRectangleIcon,
+} from '../icons';
+import { BoardTransforms } from '@plait/core';
+import React from 'react';
+import { BoardCreationMode, setCreationMode } from '@plait/common';
+import { BasicShapes, DrawPointerType } from '@plait/draw';
+import { useBoard } from '@plait/react-board';
+
+export interface ShapeProps {
+  icon: React.ReactNode;
+  title: string;
+  pointer: DrawPointerType;
+}
+
+const splitRows = (shapes: ShapeProps[], cols: number) => {
+  let result = [];
+  for (let i = 0; i < shapes.length; i += cols) {
+    result.push(shapes.slice(i, i + cols));
+  }
+  return result;
+};
+
+const SHAPES: ShapeProps[] = [
+  {
+    icon: RectangleIcon,
+    title: 'Rectangle',
+    pointer: BasicShapes.rectangle,
+  },
+  {
+    icon: EllipseIcon,
+    title: 'Ellipse',
+    pointer: BasicShapes.ellipse,
+  },
+  {
+    icon: TriangleIcon,
+    title: 'Triangle',
+    pointer: BasicShapes.triangle,
+  },
+  {
+    icon: DiamondIcon,
+    title: 'Diamond',
+    pointer: BasicShapes.diamond,
+  },
+  {
+    icon: ParallelogramIcon,
+    title: 'Parallelogram',
+    pointer: BasicShapes.parallelogram,
+  },
+  {
+    icon: RoundRectangleIcon,
+    title: 'RoundRectangle',
+    pointer: BasicShapes.roundRectangle,
+  },
+];
+
+const ROW_SHAPES = splitRows(SHAPES, 5);
+
+export type ShapePanelProps = {
+  onPointerUp: (pointer: DrawPointerType) => void;
+};
+
+export const ShapePanel: React.FC<ShapePanelProps> = ({
+  onPointerUp,
+}) => {
+  const board = useBoard();
+  return (
+    <Island padding={1}>
+      <Stack.Col gap={1}>
+        {ROW_SHAPES.map((rowShapes, rowIndex) => {
+          return (
+            <Stack.Row gap={1} key={rowIndex}>
+              {rowShapes.map((shape, index) => {
+                return (
+                  <ToolButton
+                    key={index}
+                    className={classNames({ fillable: false })}
+                    type="icon"
+                    size={'small'}
+                    visible={true}
+                    icon={shape.icon}
+                    title={shape.title}
+                    aria-label={shape.title}
+                    onPointerDown={() => {
+                      setCreationMode(board, BoardCreationMode.dnd);
+                      BoardTransforms.updatePointerType(board, shape.pointer);
+                    }}
+                    onPointerUp={() => {
+                      setCreationMode(board, BoardCreationMode.drawing);
+                      onPointerUp(shape.pointer);
+                    }}
+                  />
+                );
+              })}
+            </Stack.Row>
+          );
+        })}
+      </Stack.Col>
+    </Island>
+  );
+};
