fe213247506bd635773348cf730b28c3159d4779
pubuzhixing8 <pubuzhixing@gmail.com>
2024-12-09 19:08:15 +0800

    feat(color-picker): support merge operations for update opacity #4 (#45)
    * feat(color-picker): support merge operations for update opacity #4
    
    * feat(color-picker): support throttle for size-slider change and prevent last click after dragging
    apply setSplittingOnce to size-slider change

--- packages/drawnix/src/components/color-picker.tsx
+++ packages/drawnix/src/components/color-picker.tsx
@@ -13,21 +13,30 @@ import {
 import React from 'react';
 import { SizeSlider } from './size-slider';
 import {
+  DEFAULT_COLOR,
+  isNullOrUndefined,
+  MERGING,
+  PlaitHistoryBoard,
+} from '@plait/core';
+import {
   CLASSIC_COLORS,
   NO_COLOR,
   TRANSPARENT,
   WHITE,
 } from '../constants/color';
-import { DEFAULT_COLOR, isNullOrUndefined } from '@plait/core';
+import { useBoard } from '@plait/react-board';
 
 const ROWS_CLASSIC_COLORS = splitRows(CLASSIC_COLORS, 4);
 
 export type ColorPickerProps = {
   onSelect: (color: string) => void;
+  onColorChange?: (color: string) => void;
+  onOpacityChange?: (opacity: number) => void;
   currentColor?: string;
 };
 
 export const ColorPicker = React.forwardRef((props: ColorPickerProps, ref) => {
+  const board = useBoard();
   const { onSelect, currentColor } = props;
   const [selectedColor, setSelectedColor] = useState(
     (currentColor && removeHexAlpha(currentColor)) ||
@@ -38,55 +47,67 @@ export const ColorPicker = React.forwardRef((props: ColorPickerProps, ref) => {
     return (!isNullOrUndefined(_opacity) ? _opacity : 100) as number;
   });
   return (
-    <Stack.Col gap={3}>
-      <SizeSlider
-        step={5}
-        defaultValue={opacity}
-        onChange={(value) => {
-          setOpacity(value);
-          onSelect(applyOpacityToHex(selectedColor, value));
-        }}
-        disabled={selectedColor === CLASSIC_COLORS[0]['value']}
-      ></SizeSlider>
-      <Stack.Col gap={2}>
-        {ROWS_CLASSIC_COLORS.map((colors, index) => (
-          <Stack.Row key={index} gap={2}>
-            {colors.map((color) => {
-              return (
-                <button
-                  key={color.value}
-                  className={`color-select-item ${
-                    selectedColor === color.value ? 'active' : ''
-                  } ${isNoColor(color.value) ? 'no-color' : ''}`}
-                  style={{
-                    backgroundColor: isNoColor(color.value)
-                      ? TRANSPARENT
-                      : color.value,
-                    color: isDefaultStroke(color.value) ? WHITE : DEFAULT_COLOR,
-                  }}
-                  onClick={() => {
-                    setSelectedColor(color.value);
-                    if (color.value === NO_COLOR) {
-                      setOpacity(100);
-                      onSelect(color.value);
-                      return;
-                    }
-                    if (opacity !== 100) {
-                      onSelect(applyOpacityToHex(color.value, opacity));
-                    } else {
-                      onSelect(color.value);
-                    }
-                  }}
-                  title={color.name}
-                >
-                  {isNoColor(color.value) && NoColorIcon}
-                  {selectedColor === color.value && Check}
-                </button>
-              );
-            })}
-          </Stack.Row>
-        ))}
+    <>
+      <Stack.Col gap={3}>
+        <SizeSlider
+          step={5}
+          defaultValue={opacity}
+          onChange={(value) => {
+            setOpacity(value);
+            onSelect(applyOpacityToHex(selectedColor, value));
+          }}
+          beforeStart={() => {
+            MERGING.set(board, true);
+            // can use in @plait/core@0.74.0
+            // PlaitHistoryBoard.setSplittingOnce(board, true);
+          }}
+          afterEnd={() => {
+            MERGING.set(board, false);              
+          }}
+          disabled={selectedColor === CLASSIC_COLORS[0]['value']}
+        ></SizeSlider>
+        <Stack.Col gap={2}>
+          {ROWS_CLASSIC_COLORS.map((colors, index) => (
+            <Stack.Row key={index} gap={2}>
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color.value}
+                    className={`color-select-item ${
+                      selectedColor === color.value ? 'active' : ''
+                    } ${isNoColor(color.value) ? 'no-color' : ''}`}
+                    style={{
+                      backgroundColor: isNoColor(color.value)
+                        ? TRANSPARENT
+                        : color.value,
+                      color: isDefaultStroke(color.value)
+                        ? WHITE
+                        : DEFAULT_COLOR,
+                    }}
+                    onClick={() => {
+                      setSelectedColor(color.value);
+                      if (color.value === NO_COLOR) {
+                        setOpacity(100);
+                        onSelect(color.value);
+                        return;
+                      }
+                      if (opacity !== 100) {
+                        onSelect(applyOpacityToHex(color.value, opacity));
+                      } else {
+                        onSelect(color.value);
+                      }
+                    }}
+                    title={color.name}
+                  >
+                    {isNoColor(color.value) && NoColorIcon}
+                    {selectedColor === color.value && Check}
+                  </button>
+                );
+              })}
+            </Stack.Row>
+          ))}
+        </Stack.Col>
       </Stack.Col>
-    </Stack.Col>
+    </>
   );
 });
