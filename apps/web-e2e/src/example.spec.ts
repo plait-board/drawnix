ad71120e2d96d30a511bb2fad0795335372dce03
pubuzhixing8 <pubuzhixing@gmail.com>
2025-01-11 19:28:06 +0800

    chore: test cache

--- apps/web-e2e/src/example.spec.ts
+++ apps/web-e2e/src/example.spec.ts
@@ -4,6 +4,6 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect h1 to contain a substring.
-  expect(await page.title()).toContain('Drawnix - 开源白板工具');
+  expect(await page.title()).toContain('Drawnix - 开源白板工具1');
   expect(await page.locator('drawnix')).toBeTruthy();
 });
