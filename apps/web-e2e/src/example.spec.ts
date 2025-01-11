6c4a0f52e0344e104ed434aa841219d6b9b4d023
pubuzhixing8 <pubuzhixing@gmail.com>
2025-01-11 19:48:57 +0800

    build: e2e yml (#73)

--- apps/web-e2e/src/example.spec.ts
+++ apps/web-e2e/src/example.spec.ts
@@ -4,5 +4,6 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect h1 to contain a substring.
+  expect(await page.title()).toContain('Drawnix - 开源白板工具');
   expect(await page.locator('drawnix')).toBeTruthy();
 });
