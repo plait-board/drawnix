1b747fdbc8831375fcef38a5d333ba8bbfa6be64
pubuzhixing8 <pubuzhixing@gmail.com>
2025-01-22 18:55:51 +0800

    feat(web): add debug console

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { initializeData } from './initialize-data';
 import { Drawnix } from '@drawnix/core';
-import { PlaitElement, PlaitTheme, Viewport } from '@plait/core';
+import { PlaitBoard, PlaitElement, PlaitTheme, Viewport } from '@plait/core';
 
 const DRAWNIX_LOCAL_DATA_KEY = 'drawnix-local-data';
 
@@ -18,18 +18,41 @@ export function App() {
     return { children: initializeData };
   });
   return (
-    <Drawnix
-      value={value.children}
-      viewport={value.viewport}
-      theme={value.theme}
-      onChange={(value) => {
-        localStorage.setItem(DRAWNIX_LOCAL_DATA_KEY, JSON.stringify(value));
-      }}
-      afterInit={(board) => {
-        console.log('board initialized');
-      }}
-    ></Drawnix>
+    <>
+      <Drawnix
+        value={value.children}
+        viewport={value.viewport}
+        theme={value.theme}
+        onChange={(value) => {
+          localStorage.setItem(DRAWNIX_LOCAL_DATA_KEY, JSON.stringify(value));
+        }}
+        afterInit={(board) => {
+          console.log('board initialized');
+          console.log(
+            `add __drawnix__web__debug_log to window, so you can call add log anywhere, like: window.__drawnix__web__console('some thing')`
+          );
+          (window as any)['__drawnix__web__console'] = (value: string) => {
+            addDebugLog(board, value);
+          };
+        }}
+      ></Drawnix>
+    </>
   );
 }
+
+const addDebugLog = (board: PlaitBoard, value: string) => {
+  const container = PlaitBoard.getBoardContainer(board).closest(
+    '.drawnix'
+  ) as HTMLElement;
+  let consoleContainer = container.querySelector('.drawnix-console');
+  if (!consoleContainer) {
+    consoleContainer = document.createElement('div');
+    consoleContainer.classList.add('drawnix-console');
+    container.append(consoleContainer);
+  }
+  const div = document.createElement('div');
+  div.innerHTML = value;
+  consoleContainer.append(div);
+};
 
 export default App;
