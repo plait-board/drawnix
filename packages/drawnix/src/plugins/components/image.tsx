72e9713d533e842befdcfeb1fccd5414545b3abe
pubuzhixing8 <pubuzhixing@gmail.com>
2025-07-10 14:04:56 +0800

    feat: image viewer

--- packages/drawnix/src/plugins/components/image.tsx
+++ packages/drawnix/src/plugins/components/image.tsx
@@ -1,9 +1,19 @@
 import type { ImageProps } from '@plait/common';
+import { PlaitDrawElement } from '@plait/draw';
 
 export const Image: React.FC<ImageProps> = (props: ImageProps) => {
+  const canPreviewImage =
+    props.isFocus && !PlaitDrawElement.isImage(props.element);
+  const imgProps = {
+    className: 'image-viewer-origin',
+    src: props.imageItem.url,
+    draggable: 'false',
+    width: '100%',
+    ...(canPreviewImage === false ? { 'data-preview-disabled': true } : {}),
+  };
   return (
     <div>
-      <img src={props.imageItem.url} draggable="false" width="100%" />
+      <img {...imgProps} />
     </div>
   );
 };
